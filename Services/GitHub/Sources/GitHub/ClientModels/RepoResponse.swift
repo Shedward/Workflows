500081462ec8d465ff3ad3c63acc506d00cb6a45
Vlad Maltsev <v.maltsev@thesollution.com>
2023-07-22 19:16:46 +0300

    Fix Repo model

--- Services/GitHub/Sources/GitHub/ClientModels/RepoResponse.swift
+++ Services/GitHub/Sources/GitHub/ClientModels/RepoResponse.swift
@@ -10,4 +10,26 @@ struct RepoResponse: Decodable, RestBodyDecodable, Sendable {
     let owner: String
     let name: String
     let description: String
+
+    enum CodingKeys: CodingKey {
+        case id
+        case owner
+        case name
+        case description
+    }
+
+    enum OwnerCodingKeys: CodingKey {
+        case login
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(Int.self, forKey: .id)
+
+        let ownerContainer = try container.nestedContainer(keyedBy: OwnerCodingKeys.self, forKey: .owner)
+        self.owner = try ownerContainer.decode(String.self, forKey: .login)
+
+        self.name = try container.decode(String.self, forKey: .name)
+        self.description = try container.decode(String.self, forKey: .description)
+    }
 }
