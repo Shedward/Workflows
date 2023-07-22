a6e81aa3e5ffb8cd497f5fc957f94a614c59c65d
Vlad Maltsev <v.maltsev@thesollution.com>
2023-07-22 19:09:05 +0300

    GitHub package

---
+++ Services/GitHub/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GitHub",
+    platforms: [.macOS(.v13)],
+    products: [
+        .library(
+            name: "GitHub",
+            targets: ["GitHub"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../Core/RestClient")
+    ],
+    targets: [
+        .target(
+            name: "GitHub",
+            dependencies: ["RestClient"]
+        ),
+        .testTarget(
+            name: "GitHubTests",
+            dependencies: ["GitHub"]
+        ),
+    ]
+)
