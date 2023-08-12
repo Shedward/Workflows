e676decaeaf5c923688f5f3b2691bb64646392e7
Vlad Maltsev <v.maltsev@thesollution.com>
2023-08-12 20:00:03 +0300

    Node id for comment

--- Services/Figma/Sources/Figma/ClientModels/CommentResponse.swift
+++ Services/Figma/Sources/Figma/ClientModels/CommentResponse.swift
@@ -11,11 +11,38 @@ import RestClient
 struct CommentResponse: JSONDecodableBody {
     let id: String
     let parentId: String?
+    let nodeId: String?
     let message: String
     let createdAt: Date
     let resolvedAt: Date?
 
     static func decoder() -> JSONDecoder {
         Decoders.decoder
+    }
+
+    private enum CodingKeys: CodingKey {
+        case id
+        case parentId
+        case message
+        case createdAt
+        case resolvedAt
+        case clientMeta
+    }
+
+    private enum ClientMetaCodingKeys: CodingKey {
+        case nodeId
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.parentId = try container.decodeIfPresent(String.self, forKey: .parentId)
+        self.message = try container.decode(String.self, forKey: .message)
+        self.createdAt = try container.decode(Date.self, forKey: .createdAt)
+        self.resolvedAt = try container.decodeIfPresent(Date.self, forKey: .resolvedAt)
+
+        let clientMetaContainer = try? container.nestedContainer(keyedBy: ClientMetaCodingKeys.self, forKey: .clientMeta)
+        let nodeId = try clientMetaContainer?.decodeIfPresent(String.self, forKey: .nodeId)
+        self.nodeId = nodeId
     }
 }
--- Services/Figma/Sources/Figma/Models/Comment.swift
+++ Services/Figma/Sources/Figma/Models/Comment.swift
@@ -9,11 +9,15 @@ public struct Comment {
     private let client: FigmaClient
 
     public let id: String
+    public let parentId: String?
+    public let nodeId: String?
     public let message: String
 
     init(response: CommentResponse, client: FigmaClient) {
         self.client = client
         self.id = response.id
+        self.nodeId = response.nodeId
+        self.parentId = response.parentId
         self.message = response.message
     }
 }
