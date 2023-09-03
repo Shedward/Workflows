027ececb151434b3c510fda554987e2e7349a875
Vlad Maltsev <v.maltsev@hh.ru>
2023-09-03 17:06:34 +0300

    Basic layout

--- Apps/Workflows/WorkflowsApp/Sources/WorkflowsApp/UI/WorkflowsApp.swift
+++ Apps/Workflows/WorkflowsApp/Sources/WorkflowsApp/UI/WorkflowsApp.swift
@@ -1,8 +1,34 @@
 //
-//  File.swift
-//  
+//  WorkflowsApp.swift
+//
 //
 //  Created by v.maltsev on 03.09.2023.
 //
 
-import Foundation
+import SwiftUI
+
+public struct WorkflowsApp: App {
+
+    public init() {
+    }
+
+    public var body: some Scene {
+        MenuBarExtra {
+            VStack(alignment: .leading) {
+                Text("Workflows")
+                    .font(.title)
+                Text("Here will be all your work")
+                Button("Quit") {
+                    NSApplication.shared.terminate(nil)
+                }
+            }
+            .padding()
+        } label: {
+            HStack {
+                Image(systemName: "flowchart")
+                Text("WF")
+            }
+        }
+        .menuBarExtraStyle(.window)
+    }
+}
